d07dc2e2c78fa93b2da4941b0892804695b7c3ef
dante0432 <jgcardenasa@gmail.com>
2021-07-14 23:44:03 -0500

    Invoice a6

--- symfony/src/Controller/SaleController.php
+++ symfony/src/Controller/SaleController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**
  * @Route("/sale")
@@ -29,18 +31,51 @@ class SaleController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/toggleCancelle", name="sale_cancelle", methods={"GET","POST"})
-     */
-     public function toggleCancelle(SaleRepository $saleRepository, $id): Response
-     {
-        $sale = $saleRepository->findOneBy(['id' => $id]);
-        $sale->setCancelled(!$sale->getCancelled());
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($sale);
-        $entityManager->flush();
-        return $this->redirectToRoute('sale_index');
+    * @Route("/{id}/toggleCancelle", name="sale_cancelle", methods={"GET","POST"})
+    */
+    public function toggleCancelle(SaleRepository $saleRepository, $id): Response
+    {
+    $sale = $saleRepository->findOneBy(['id' => $id]);
+    $sale->setCancelled(!$sale->getCancelled());
+    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager->persist($sale);
+    $entityManager->flush();
+    return $this->redirectToRoute('sale_index');
 
-     }
+    }
+
+    /**
+     * @Route("/{id}/invoice", name="sale_invoice", methods={"GET","POST"})
+     */
+    public function invoice(SaleRepository $saleRepository, Sale $sale): Response
+    {
+        // Configure Dompdf según sus necesidades
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Robooto');
+        
+        // Crea una instancia de Dompdf con nuestras opciones
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Recupere el HTML generado en nuestro archivo twig
+        $html = $this->renderView('sale/invoice.html.twig', [
+            'sale' => $sale,
+        ]);
+        
+        // Cargar HTML en Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Opcional) Configure el tamaño del papel y la orientación 'vertical' o 'vertical'
+        $dompdf->setPaper('A6', 'portrait');
+
+        // Renderiza el HTML como PDF
+        $dompdf->render();
+
+        // Envíe el PDF generado al navegador (descarga forzada)
+        $dompdf->stream("invoice_".$sale->getId()."_".date('dmyyyy').".pdf", [
+            "Attachment" => true
+        ]);
+        return $this->redirectToRoute('sale_index');
+    }
 
     /**
      * @Route("/new/{productId}", name="sale_new", methods={"GET","POST"})
